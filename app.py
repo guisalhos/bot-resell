5147fe68495cfe2ea50e4b3eba55685dcef684a1
Guisalhos <guirainhamm@gmail.com>
2026-04-06 00:20:14 +0100

    fix database bug

--- app.py
+++ app.py
@@ -18,13 +18,15 @@ from motor import bot_inicio, bot_prompt, bot_processar
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
-database_url = os.getenv("DATABASE_URL", "sqlite:///local_stock.db")
+database_url = os.getenv("DATABASE_URL")
+
+if not database_url:
+    raise ValueError("DATABASE_URL não está definida.")
 
 if database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
